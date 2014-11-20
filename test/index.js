e2decdd737bd1f65abb7a23dbaff276ba90ffda1
Vadim Goncharov <vg@vadimgoncharov.ru>
2014-11-20 21:28:52 +0300

    Fix tests

--- test/index.js
+++ test/index.js
@@ -3,18 +3,15 @@
 const WatcherNonInterruptibleError = require('../').WatcherNonInterruptibleError;
 const expect = require('chai').expect;
 const VError = require('verror');
-const sprintf = require('extsprintf').sprintf;
 
 describe('WatcherNonInterruptibleError', function() {
+	it('should be instanceOf VError', function() {
+		expect(new WatcherNonInterruptibleError()).to.be.instanceof(VError);
+	});
+
 	it('should throw WatcherNonInterruptibleError and to be instanceOf it', function() {
 		expect(function() {
-			try {
-				throw new WatcherNonInterruptibleError(new Error('upper level'), 'lower level');
-			}
-			catch (error) {
-				throw new WatcherNonInterruptibleError('non-fatal error');
-			}
-
+			throw new WatcherNonInterruptibleError('non-fatal error');
 		})
 			.to.be.throw(WatcherNonInterruptibleError)
 			.and
